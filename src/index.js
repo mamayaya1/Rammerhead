177ddf549214cb0f84ff54db6eee19aea1671f02
mamayaya1 <99693981+mamayaya1@users.noreply.github.com>
2026-05-24 14:35:30 -0400

    Refactor server CORS handling and logging
    Removed CORS middleware and related headers from the server setup. Updated the logging for server address and improved code formatting.

--- src/index.js
+++ src/index.js
@@ -10,82 +10,59 @@ import connect from "connect";
 console.log("Rammerhead easy deployment version\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
 
 const app = connect();
-// CORS FIX FOR FRONTEND ON DIFFERENT DOMAIN
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    if (req.method === "OPTIONS") {
-        res.writeHead(200);
-        res.end();
-        return;
-    }
-    next();
-});
-
 const rh = createRammerhead();
 const server = createServer();
 
 // used when forwarding the script
 const rammerheadScopes = [
-	"/rammerhead.js",
-	"/hammerhead.js",
-	"/transport-worker.js",
-	"/task.js",
-	"/iframe-task.js",
-	"/worker-hammerhead.js",
-	"/messaging",
-	"/sessionexists",
-	"/deletesession",
-	"/newsession",
-	"/editsession",
-	"/needpassword",
-	"/syncLocalStorage",
-	"/api/shuffleDict"
+    "/rammerhead.js",
+    "/hammerhead.js",
+    "/transport-worker.js",
+    "/task.js",
+    "/iframe-task.js",
+    "/worker-hammerhead.js",
+    "/messaging",
+    "/sessionexists",
+    "/deletesession",
+    "/newsession",
+    "/editsession",
+    "/needpassword",
+    "/syncLocalStorage",
+    "/api/shuffleDict"
 ];
 const rammerheadSession = /^\/[a-z0-9]{32}/;
 
 function shouldRouteRh(req) {
-	const url = new URL(req.url, "http://0.0.0.0");
-	return (rammerheadScopes.includes(url.pathname) || rammerheadSession.test(url.pathname));
+    const url = new URL(req.url, "http://0.0.0.0");
+    return (rammerheadScopes.includes(url.pathname) || rammerheadSession.test(url.pathname));
 }
 
 app.use((req, res, next) => {
-    if(shouldRouteRh(req)) rh.emit("request", req, res); else next();
+    if (shouldRouteRh(req)) rh.emit("request", req, res);
+    else next();
 });
 
+// serve static frontend (your index.html, script.js, api.js, etc.)
 app.use(serveStatic(fileURLToPath(new URL("../static/", import.meta.url))));
 
 server.on("request", app);
-server.on("upgrade", (req, socket, head) => {
-    // Add CORS headers for WebSocket upgrade
-    try {
-        socket.write(
-            "HTTP/1.1 101 Switching Protocols\r\n" +
-            "Access-Control-Allow-Origin: *\r\n" +
-            "Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n" +
-            "Access-Control-Allow-Headers: Content-Type\r\n" +
-            "\r\n"
-        );
-    } catch (e) {}
 
-    if (shouldRouteRh(req)) {
-        rh.emit("upgrade", req, socket, head);
-    } else {
-        socket.end();
-    }
+server.on("upgrade", (req, socket, head) => {
+    if (shouldRouteRh(req)) rh.emit("upgrade", req, socket, head);
+    else socket.end();
 });
 
 server.on("listening", () => {
-  const addr = server.address();
+    const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}`)
-  console.log("");
-  console.log("You can now view it in your browser.")
-  /* Code for listing IPS from website-aio */
-  console.log(`Local: http://${addr.family === "IPv6" ? `[${addr.address}]` : addr.address}${addr.port === 80? "" : ":" + addr.port}`);
-  console.log(`Local: http://localhost${addr.port === 80? "" : ":" + addr.port}`);
-  try { console.log(`On Your Network: http://${hostname()}${addr.port === 80? "" : ":" + addr.port}`); } catch (err) {/* Can't find LAN interface */};
+    console.log(`Server running on port ${addr.port}`);
+    console.log("");
+    console.log("You can now view it in your browser.");
+    console.log(`Local: http://${addr.family === "IPv6" ? `[${addr.address}]` : addr.address}${addr.port === 80 ? "" : ":" + addr.port}`);
+    console.log(`Local: http://localhost${addr.port === 80 ? "" : ":" + addr.port}`);
+    try {
+        console.log(`On Your Network: http://${hostname()}${addr.port === 80 ? "" : ":" + addr.port}`);
+    } catch (err) { /* Can't find LAN interface */ }
 });
 
-server.listen({ port: process.env.PORT || 8080 })
+server.listen({ port: process.env.PORT || 8080 });
